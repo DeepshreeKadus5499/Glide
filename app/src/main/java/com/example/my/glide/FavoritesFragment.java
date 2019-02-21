315b446f251066e0e30a72b745604d2625827860
DeepshreeKadus5499 <kadusdeepshree5499@gmail.com>
2019-02-21 19:55:09 +0530

    ...

---
+++ app/src/main/java/com/example/my/glide/FavoritesFragment.java
@@ -0,0 +1,24 @@
+package com.example.my.glide;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Samrudhi Ghorpade on 17-02-2019.
+ */
+
+public class FavoritesFragment extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_favorites, container, false);
+    }
+}
